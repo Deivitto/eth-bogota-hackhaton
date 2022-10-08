2814043ce0a8cf909fbf3a85700b35c339401dab
Baraa42 <75384314+Baraa42@users.noreply.github.com>
2022-10-08 18:56:08 -0500

    update contracts and packages

--- contracts/SmartCoWOrder.sol
+++ contracts/SmartCoWOrder.sol
@@ -8,36 +8,53 @@ import {ICoWSwapSettlement} from "./interfaces/ICoWSwapSettlement.sol";
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import {IERC1271} from "@openzeppelin/contracts/interfaces/IERC1271.sol";
 import {CFAv1Library} from "@superfluid-finance/ethereum-contracts/contracts/apps/CFAv1Library.sol";
+import {GPv2Order} from "./vendored/GPv2Order.sol";
+import {ICoWSwapOnchainOrders} from "./vendored/ICoWSwapOnchainOrders.sol";
+import {AutomationCompatibleInterface} from "@chainlink/contracts/src/v0.8/interfaces/AutomationCompatibleInterface.sol";
 
-contract SmartCoWOrder is IERC1271 {
+contract SmartCoWOrder is IERC1271, ICoWSwapOnchainOrders, AutomationCompatibleInterface {
+    using GPv2Order for *;
+    using CFAv1Library for CFAv1Library.InitData;
+
     bytes4 constant ERC1271_MAGIC_VALUE = 0x1626ba7e;
+    bytes32 public constant APP_DATA = keccak256("Smart DCA");
 
     address public immutable owner;
-    uint256 public dcaAmount;
     bytes32 public orderHash;
-    IERC20 public immutable sellToken;
+    bytes32 public immutable domainSeparator;
     ISuperToken public immutable superToken;
-    using CFAv1Library for CFAv1Library.InitData;
+
     CFAv1Library.InitData public cfaV1;
+    Data public data;
+
+    bool isOpen = true;
+
+    struct Data {
+        IERC20 sellToken;
+        IERC20 buyToken;
+        address receiver;
+        uint256 sellAmount;
+        uint256 buyAmount;
+        uint32 deadline;
+        uint256 feeAmount;
+        bytes meta;
+    }
 
     event Downgraded(uint256 amount);
+    event OrderUID(bytes orderUID);
 
     constructor(
         address owner_,
         ISuperfluid host,
-        IERC20 sellToken_,
         ISuperToken superToken_,
-        uint256 dcaAmount_,
-        bytes32 orderHash_,
-        ICoWSwapSettlement settlement
+        ICoWSwapSettlement settlement,
+        Data memory data_
     ) {
         owner = owner_;
-        sellToken = sellToken_;
         superToken = superToken_;
-        dcaAmount = dcaAmount_;
-        orderHash = orderHash_;
-
-        sellToken_.approve(settlement.vaultRelayer(), type(uint256).max);
+        data = data_;
+        domainSeparator = settlement.domainSeparator();
+        IERC20(data.sellToken).approve(settlement.vaultRelayer(), type(uint256).max);
         cfaV1 = CFAv1Library.InitData(
             host,
             IConstantFlowAgreementV1(
@@ -50,16 +67,57 @@ contract SmartCoWOrder is IERC1271 {
         );
     }
 
-    function updateAmount(uint256 newDcaAmount) external {
+    function updateData(Data memory data_) external {
         require(msg.sender == owner, "not the owner");
-        dcaAmount = newDcaAmount;
+        require(
+            data.sellToken == data_.sellToken && data.buyToken == data_.buyToken,
+            "Cant change tokens"
+        );
+        data = data_;
     }
 
-    function downgrade() external {
-        uint256 balance = superToken.balanceOf(address(this));
-        require(balance >= dcaAmount, "small balance");
-        superToken.downgrade(dcaAmount);
-        emit Downgraded(dcaAmount);
+    function performUpkeep(
+        bytes calldata /* performData */
+    ) external override {
+        require(
+            isOpen && (superToken.balanceOf(address(this)) >= data.sellAmount),
+            "upKeep not needed"
+        );
+        superToken.downgrade(data.sellAmount);
+        emit Downgraded(data.sellAmount);
+
+        GPv2Order.Data memory order = GPv2Order.Data({
+            sellToken: data.sellToken,
+            buyToken: data.buyToken,
+            receiver: data.receiver == GPv2Order.RECEIVER_SAME_AS_OWNER ? owner : data.receiver,
+            sellAmount: data.sellAmount,
+            buyAmount: data.buyAmount,
+            validTo: uint32(block.timestamp) + data.deadline,
+            appData: APP_DATA,
+            feeAmount: data.feeAmount,
+            kind: GPv2Order.KIND_SELL,
+            partiallyFillable: false,
+            sellTokenBalance: GPv2Order.BALANCE_ERC20,
+            buyTokenBalance: GPv2Order.BALANCE_ERC20
+        });
+        orderHash = order.hash(domainSeparator);
+        OnchainSignature memory signature = OnchainSignature({
+            scheme: OnchainSigningScheme.Eip1271,
+            data: hex""
+        });
+
+        emit OrderPlacement(address(this), order, signature, data.meta);
+
+        bytes memory orderUid = new bytes(GPv2Order.UID_LENGTH);
+        orderUid.packOrderUidParams(orderHash, address(this), order.validTo);
+        emit OrderUID(orderUid);
+    }
+
+    function checkUpkeep(
+        bytes calldata /* checkData */
+    ) public view override returns (bool upkeepNeeded, bytes memory performData) {
+        upkeepNeeded = isOpen && superToken.balanceOf(address(this)) >= data.sellAmount;
+        performData = "";
     }
 
     function isValidSignature(bytes32 orderHash_, bytes calldata)
@@ -68,15 +126,18 @@ contract SmartCoWOrder is IERC1271 {
         returns (bytes4 magicValue)
     {
         require(orderHash_ == orderHash, "invalid order");
-        require(sellToken.balanceOf(address(this)) >= dcaAmount, "low balance");
+        require(IERC20(data.sellToken).balanceOf(address(this)) >= data.sellAmount, "low balance");
         magicValue = ERC1271_MAGIC_VALUE;
     }
 
     function cancel() public {
         require(msg.sender == owner, "not the owner");
+        require(isOpen, "Already closed");
         orderHash = bytes32(0);
         cfaV1.deleteFlow(owner, address(this), superToken);
+        IERC20 sellToken = IERC20(data.sellToken);
         sellToken.transfer(owner, sellToken.balanceOf(address(this)));
         superToken.transfer(owner, superToken.balanceOf(address(this)));
+        isOpen = false;
     }
 }
--- contracts/SmartCoWOrders.sol
+++ contracts/SmartCoWOrders.sol
@@ -13,41 +13,23 @@ import {ICoWSwapSettlement} from "./interfaces/ICoWSwapSettlement.sol";
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import {GPv2Order} from "./vendored/GPv2Order.sol";
 import {SmartCoWOrder} from "./SmartCoWOrder.sol";
-import {ICoWSwapOnchainOrders} from "./vendored/ICoWSwapOnchainOrders.sol";
 
 // Create DCA order
 // Order: owner, DCA amount, min frequency
 //
 
-contract SmartCoWOrder2 is ICoWSwapOnchainOrders {
+contract SmartCoWOrder2 {
     using GPv2Order for *;
     ISuperfluid host;
     using CFAv1Library for CFAv1Library.InitData;
     CFAv1Library.InitData public cfaV1;
-
-    struct Data {
-        IERC20 sellToken;
-        IERC20 buyToken;
-        address receiver;
-        uint256 sellAmount;
-        uint256 buyAmount;
-        uint32 validFrom;
-        uint32 validTo;
-        uint256 feeAmount;
-        bytes meta;
-    }
-
-    bytes32 public constant APP_DATA = keccak256("Smart DCA");
-
     ICoWSwapSettlement public immutable settlement;
-    bytes32 public immutable domainSeparator;
 
     mapping(address => address) public addressToContract;
 
     constructor(ISuperfluid host_, ICoWSwapSettlement settlement_) {
         settlement = settlement_;
         host = host_;
-        domainSeparator = settlement_.domainSeparator();
         // Initialize CFA Library
         cfaV1 = CFAv1Library.InitData(
             host,
@@ -62,53 +44,22 @@ contract SmartCoWOrder2 is ICoWSwapOnchainOrders {
     }
 
     function place(
-        Data calldata data,
+        SmartCoWOrder.Data calldata data,
         ISuperToken superToken,
         int96 flowRate,
         bytes32 salt
-    ) external returns (bytes memory orderUid) {
+    ) external {
         require(superToken.getUnderlyingToken() == address(data.sellToken), "!SuperToken");
-        GPv2Order.Data memory order = GPv2Order.Data({
-            sellToken: data.sellToken,
-            buyToken: data.buyToken,
-            receiver: data.receiver == GPv2Order.RECEIVER_SAME_AS_OWNER
-                ? msg.sender
-                : data.receiver,
-            sellAmount: data.sellAmount,
-            buyAmount: data.buyAmount,
-            validTo: data.validTo,
-            appData: APP_DATA,
-            feeAmount: data.feeAmount,
-            kind: GPv2Order.KIND_SELL,
-            partiallyFillable: false,
-            sellTokenBalance: GPv2Order.BALANCE_ERC20,
-            buyTokenBalance: GPv2Order.BALANCE_ERC20
-        });
-        bytes32 orderHash = order.hash(domainSeparator);
-
         SmartCoWOrder instance = new SmartCoWOrder{salt: salt}(
             msg.sender,
             host,
-            data.sellToken,
             superToken,
-            data.sellAmount,
-            orderHash,
-            settlement
+            settlement,
+            data
         );
 
         /** @dev: Require this contract to be a flowOperator for msg.sender */
         cfaV1.createFlowByOperator(msg.sender, address(instance), superToken, flowRate);
-
-        OnchainSignature memory signature = OnchainSignature({
-            scheme: OnchainSigningScheme.Eip1271,
-            data: hex""
-        });
-
-        emit OrderPlacement(address(instance), order, signature, data.meta);
-
-        orderUid = new bytes(GPv2Order.UID_LENGTH);
-        orderUid.packOrderUidParams(orderHash, address(instance), data.validTo);
-
         addressToContract[msg.sender] = address(instance);
     }
 }
