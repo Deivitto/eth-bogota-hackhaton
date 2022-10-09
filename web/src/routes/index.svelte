d4d1211621f4147a8e7fe376228e992d60335978
Deivitto <info.dcm62@gmail.com>
2022-10-09 03:27:22 +0200

    big css changes

--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -5,7 +5,6 @@
 <script>
     import { connected, signerAddress, chainId } from "svelte-ethers-store"
     import SvgCheck from "$lib/svgCheck.svelte"
-    import Background from "./Background.svelte"
 
     // import { main } from "$lib/placeOrder"
     import { onConnect, onDisconnect, connectWallet } from "$lib/web3"
@@ -52,7 +51,7 @@
 
 <div class="index">
     <div
-        class="from-primary to-secondary text-primary-content -mt-[4rem] grid place-items-center items-end bg-gradient-to-br pt-20"
+        class="from-primary to-secondary text-primary-content -mt-[4rem] grid place-items-center items-end bg-gradient-to-br pt-20 hero-image-main"
     >
         <div
             class="hero-content col-start-1 row-start-1 w-full max-w-7xl flex-col justify-between gap-10 pb-20 lg:flex-row lg:items-end lg:gap-0 xl:gap-20"
@@ -137,7 +136,7 @@
                         </button> -->
                         <a
                             href="/swap"
-                            class="btn button-first-section btn-ghost btn-active lg:btn-lg normal-case"
+                            class="btn button-first-section btn-ghost btn-active lg:btn-lg normal-case "
                         >
                             Swap
                         </a>
@@ -167,7 +166,8 @@
                 /> -->
             </div>
         </div>
-        <!-- <Background /> -->
+
+        <!-- <Background /> https://cdn.pixabay.com/photo/2016/11/18/12/55/light-1834289_960_720.jpg-->
         <!-- <svg
             class="fill-secondary col-start-1 row-start-1 h-auto w-full"
             width="1600"
@@ -184,7 +184,9 @@
 
     <!-- {$signerAddress} -->
 
-    <div class="hero bg-base-100 text-base-content mx-auto min-h-screen max-w-md md:max-w-full">
+    <div
+        class="hero bg-base-100 text-base-content mx-auto min-h-screen max-w-md md:max-w-full background-regular"
+    >
         <div class="hero-content px-4 text-center md:px-0">
             <div id="how">
                 <h2 class="mt-32 mb-2 text-4xl font-extrabold md:text-6xl">How does it work?</h2>
@@ -222,7 +224,7 @@
     </div>
 
     <div
-        class="hero from-primary to-accent text-primary-content min-h-screen bg-gradient-to-br"
+        class="hero from-primary to-accent text-primary-content min-h-screen bg-gradient-to-br background-gradient"
         id="private"
     >
         <div class="hero-content mx-auto max-w-md text-center md:max-w-full">
@@ -234,7 +236,7 @@
                 <p class="mx-auto mb-5 w-full regular-text lh-regular">LOREM IPSUM</p>
 
                 <div class="my-20 flex flex-col custom-button-centered">
-                    <a class="btn btn-tertiary  rounded-full" href="/create-sponsor"
+                    <a class="btn btn-tertiary   rounded-full" href="/create-sponsor"
                         >Create your first CoW transaction!</a
                     >
                 </div>
@@ -243,7 +245,7 @@
     </div>
 
     <div
-        class="hero from-primary to-accent text-primary-content min-h-screen bg-gradient-to-br"
+        class="hero from-primary to-accent text-primary-content min-h-screen bg-gradient-to-br background-regular"
         id="private"
     >
         <div class="hero-content mx-auto max-w-md text-center md:max-w-full">
@@ -261,12 +263,12 @@
                     class="my-20 flex flex-col md:flex-row custom-button-centered space-between md:gap-4"
                 >
                     <a
-                        class="btn  btn-tertiary btn-outline rounded-full w-full md:w-1/2"
+                        class="btn  btn-primary btn-outline rounded-full w-full md:w-1/2 btn-fix"
                         href="/mistery-transactions">Mistery button</a
                     >
                     <br />
                     <a
-                        class="btn btn-primary rounded-full w-full md:w-1/2 "
+                        class="btn btn-primary rounded-full w-full md:w-1/2 btn-fix"
                         href="/browse-transactions"
                     >
                         Browse transactions!</a
@@ -278,6 +280,15 @@
 </div>
 
 <style>
+    * {
+        --primary: rgb(214, 123, 90) !important;
+        --secondary: rgb(22, 56, 97) !important;
+        --primary-light: rgba(214, 123, 90, 0.375) !important;
+        --primary-hover: rgb(237, 137, 100) !important;
+        --secondary-light: rgba(21, 55, 96, 0.375) !important;
+        --secondary-hover: rgb(17, 45, 78) !important;
+    }
+
     .step {
         text-align: left;
     }
@@ -452,5 +463,414 @@
         .button-first-section {
             min-width: 40%;
         }
+    }
+
+    .hero-image-main {
+        /* background-image: url("web/static/skyscrapers-4168483.jpg"); */
+        background-size: cover;
+        background-image: url("skyscrapers-4168483.jpg");
+        background-repeat: no-repeat;
+    }
+
+    .hero-content > div {
+        background: rgba(255, 255, 255, 0.03);
+        border-radius: 16px;
+        box-shadow: 0 4px 30px rgb(0 0 0 / 10%);
+        backdrop-filter: blur(6.9px);
+        -webkit-backdrop-filter: blur(6.9px);
+        border: 1px solid rgba(255, 255, 255, 0.25);
+        margin-left: 1rem;
+        margin-right: 1rem;
+        padding: 1.2rem;
+    }
+
+    .hero-image-main * {
+        color: white !important;
+    }
+
+    .background-gradient {
+        -webkit-text-size-adjust: 100%;
+        tab-size: 4;
+        font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Helvetica Neue, Arial, Noto Sans, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+            Segoe UI Symbol, "Noto Color Emoji";
+        -webkit-tap-highlight-color: transparent;
+        --p: 262 80% 50%;
+        --pf: 262 80% 40%;
+        --sf: 316 70% 40%;
+        --af: 175 70% 33%;
+        --in: 198 93% 60%;
+        --su: 158 64% 52%;
+        --wa: 43 96% 56%;
+        --er: 0 91% 71%;
+        --inc: 198 100% 12%;
+        --suc: 158 100% 10%;
+        --wac: 43 100% 11%;
+        --erc: 0 100% 14%;
+        --rounded-box: 1rem;
+        --rounded-btn: 0.5rem;
+        --rounded-badge: 1.9rem;
+        --animation-btn: 0.25s;
+        --animation-input: 0.2s;
+        --btn-text-case: uppercase;
+        --btn-focus-scale: 0.95;
+        --border-btn: 1px;
+        --tab-border: 1px;
+        --tab-radius: 0.5rem;
+        --pc: 0 0% 100%;
+        --s: 316 70% 50%;
+        --sc: 0 0% 100%;
+        --a: 175 70% 41%;
+        --ac: 0 0% 100%;
+        --n: 218 18% 12%;
+        --nf: 223 17% 8%;
+        --nc: 220 13% 69%;
+        --b1: 220 18% 20%;
+        --b2: 220 17% 17%;
+        --b3: 219 18% 15%;
+        --bc: 220 13% 69%;
+        line-height: inherit;
+        box-sizing: border-box;
+        border-width: 0;
+        border-style: solid;
+        border-color: #e5e7eb;
+        --tw-translate-x: 0;
+        --tw-translate-y: 0;
+        --tw-rotate: 0;
+        --tw-skew-x: 0;
+        --tw-skew-y: 0;
+        --tw-scale-x: 1;
+        --tw-scale-y: 1;
+        --tw-scroll-snap-strictness: proximity;
+        --tw-ring-offset-width: 0px;
+        --tw-ring-offset-color: #fff;
+        --tw-ring-color: rgb(59 130 246 / 0.5);
+        --tw-ring-offset-shadow: 0 0 #0000;
+        --tw-ring-shadow: 0 0 #0000;
+        --tw-shadow: 0 0 #0000;
+        --tw-shadow-colored: 0 0 #0000;
+        display: grid;
+        width: 100%;
+        place-items: center;
+        background-size: cover;
+        background-position: center;
+        min-height: 100vh;
+        background-image: linear-gradient(to bottom right, var(--tw-gradient-stops));
+        --tw-gradient-from: hsl(var(--p));
+        --tw-gradient-stops: var(--tw-gradient-from), var(--tw-gradient-to, hsl(var(--p) / 0));
+        --tw-gradient-to: hsl(var(--a));
+        --tw-text-opacity: 1;
+        color: hsl(var(--pc) / var(--tw-text-opacity));
+        background: rgb(214, 123, 90);
+        background: linear-gradient(
+            180deg,
+            rgba(42, 48, 60, 1) 0%,
+            rgba(22, 56, 97, 1) 25%,
+            /* rgba(214, 123, 90, 1) 30%, */ rgba(22, 56, 97, 1) 75%,
+            rgba(42, 48, 60, 1) 100%
+        );
+    }
+
+    .background-regular {
+        -webkit-text-size-adjust: 100%;
+        tab-size: 4;
+        font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Helvetica Neue, Arial, Noto Sans, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+            Segoe UI Symbol, "Noto Color Emoji";
+        -webkit-tap-highlight-color: transparent;
+        --p: 262 80% 50%;
+        --pf: 262 80% 40%;
+        --sf: 316 70% 40%;
+        --af: 175 70% 33%;
+        --in: 198 93% 60%;
+        --su: 158 64% 52%;
+        --wa: 43 96% 56%;
+        --er: 0 91% 71%;
+        --inc: 198 100% 12%;
+        --suc: 158 100% 10%;
+        --wac: 43 100% 11%;
+        --erc: 0 100% 14%;
+        --rounded-box: 1rem;
+        --rounded-btn: 0.5rem;
+        --rounded-badge: 1.9rem;
+        --animation-btn: 0.25s;
+        --animation-input: 0.2s;
+        --btn-text-case: uppercase;
+        --btn-focus-scale: 0.95;
+        --border-btn: 1px;
+        --tab-border: 1px;
+        --tab-radius: 0.5rem;
+        --pc: 0 0% 100%;
+        --s: 316 70% 50%;
+        --sc: 0 0% 100%;
+        --a: 175 70% 41%;
+        --ac: 0 0% 100%;
+        --n: 218 18% 12%;
+        --nf: 223 17% 8%;
+        --nc: 220 13% 69%;
+        --b1: 220 18% 20%;
+        --b2: 220 17% 17%;
+        --b3: 219 18% 15%;
+        --bc: 220 13% 69%;
+        line-height: inherit;
+        box-sizing: border-box;
+        border-width: 0;
+        border-style: solid;
+        border-color: #e5e7eb;
+        --tw-translate-x: 0;
+        --tw-translate-y: 0;
+        --tw-rotate: 0;
+        --tw-skew-x: 0;
+        --tw-skew-y: 0;
+        --tw-scale-x: 1;
+        --tw-scale-y: 1;
+        --tw-scroll-snap-strictness: proximity;
+        --tw-ring-offset-width: 0px;
+        --tw-ring-offset-color: #fff;
+        --tw-ring-color: rgb(59 130 246 / 0.5);
+        --tw-ring-offset-shadow: 0 0 #0000;
+        --tw-ring-shadow: 0 0 #0000;
+        --tw-shadow: 0 0 #0000;
+        --tw-shadow-colored: 0 0 #0000;
+        display: grid;
+        width: 100%;
+        place-items: center;
+        background-size: cover;
+        background-position: center;
+        margin-left: auto;
+        margin-right: auto;
+        min-height: 100vh;
+        --tw-bg-opacity: 1;
+        background-color: hsl(var(--b1) / var(--tw-bg-opacity));
+        --tw-text-opacity: 1;
+        color: hsl(var(--bc) / var(--tw-text-opacity));
+        max-width: 100%;
+
+        padding: 3rem;
+    }
+
+    .btn-fix {
+        -webkit-text-size-adjust: 100%;
+        tab-size: 4;
+        font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Helvetica Neue, Arial, Noto Sans, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+            Segoe UI Symbol, "Noto Color Emoji";
+        -webkit-tap-highlight-color: transparent;
+        --p: 262 80% 50%;
+        --pf: 262 80% 40%;
+        --sf: 316 70% 40%;
+        --af: 175 70% 33%;
+        --in: 198 93% 60%;
+        --su: 158 64% 52%;
+        --wa: 43 96% 56%;
+        --er: 0 91% 71%;
+        --inc: 198 100% 12%;
+        --suc: 158 100% 10%;
+        --wac: 43 100% 11%;
+        --erc: 0 100% 14%;
+        --rounded-box: 1rem;
+        --rounded-btn: 0.5rem;
+        --rounded-badge: 1.9rem;
+        --animation-btn: 0.25s;
+        --animation-input: 0.2s;
+        --btn-text-case: uppercase;
+        --btn-focus-scale: 0.95;
+        --border-btn: 1px;
+        --tab-border: 1px;
+        --tab-radius: 0.5rem;
+        --pc: 0 0% 100%;
+        --s: 316 70% 50%;
+        --sc: 0 0% 100%;
+        --a: 175 70% 41%;
+        --ac: 0 0% 100%;
+        --n: 218 18% 12%;
+        --nf: 223 17% 8%;
+        --nc: 220 13% 69%;
+        --b1: 220 18% 20%;
+        --b2: 220 17% 17%;
+        --b3: 219 18% 15%;
+        --bc: 220 13% 69%;
+        --tw-gradient-from: hsl(var(--p));
+        --tw-gradient-stops: var(--tw-gradient-from), var(--tw-gradient-to, hsl(var(--p) / 0));
+        --tw-gradient-to: hsl(var(--a));
+        box-sizing: border-box;
+        border-style: solid;
+        --tw-translate-x: 0;
+        --tw-translate-y: 0;
+        --tw-rotate: 0;
+        --tw-skew-x: 0;
+        --tw-skew-y: 0;
+        --tw-scale-x: 1;
+        --tw-scale-y: 1;
+        --tw-pan-x: ;
+        --tw-pan-y: ;
+        --tw-pinch-zoom: ;
+        --tw-scroll-snap-strictness: proximity;
+        --tw-ordinal: ;
+        --tw-slashed-zero: ;
+        --tw-numeric-figure: ;
+        --tw-numeric-spacing: ;
+        --tw-numeric-fraction: ;
+        --tw-ring-inset: ;
+        --tw-ring-offset-width: 0px;
+        --tw-ring-offset-color: #fff;
+        --tw-ring-color: rgb(59 130 246 / 0.5);
+        --tw-ring-offset-shadow: 0 0 #0000;
+        --tw-ring-shadow: 0 0 #0000;
+        --tw-shadow: 0 0 #0000;
+        --tw-shadow-colored: 0 0 #0000;
+        --tw-blur: ;
+        --tw-brightness: ;
+        --tw-contrast: ;
+        --tw-grayscale: ;
+        --tw-hue-rotate: ;
+        --tw-invert: ;
+        --tw-saturate: ;
+        --tw-sepia: ;
+        --tw-drop-shadow: ;
+        --tw-backdrop-blur: ;
+        --tw-backdrop-brightness: ;
+        --tw-backdrop-contrast: ;
+        --tw-backdrop-grayscale: ;
+        --tw-backdrop-hue-rotate: ;
+        --tw-backdrop-invert: ;
+        --tw-backdrop-opacity: ;
+        --tw-backdrop-saturate: ;
+        --tw-backdrop-sepia: ;
+        text-decoration: inherit;
+        display: inline-flex;
+        flex-shrink: 0;
+        cursor: pointer;
+        user-select: none;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        transition-property: color, background-color, border-color, text-decoration-color, fill,
+            stroke, opacity, box-shadow, transform, filter, backdrop-filter,
+            -webkit-text-decoration-color, -webkit-backdrop-filter;
+        transition-duration: 0.2s;
+        transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+        height: 3rem;
+        padding-left: 1rem;
+        padding-right: 1rem;
+        font-size: 0.875rem;
+        line-height: 1em;
+        min-height: 3rem;
+        font-weight: 600;
+        text-transform: var(--btn-text-case, uppercase);
+        text-decoration-line: none;
+        border-width: var(--border-btn, 1px);
+        animation: button-pop var(--animation-btn, 0.25s) ease-out;
+        --tw-border-opacity: 1;
+        border-color: hsl(var(--p) / var(--tw-border-opacity));
+        --tw-bg-opacity: 1;
+        background-color: hsl(var(--p) / var(--tw-bg-opacity));
+        --tw-text-opacity: 1;
+        color: hsl(var(--pc) / var(--tw-text-opacity));
+        border-radius: 9999px;
+        width: 50%;
+    }
+
+    .btn-primary {
+        background-color: var(--primary) !important;
+        border-color: var(--primary) !important;
+    }
+
+    .btn-primary:hover {
+        background-color: var(--primary-hover) !important;
+        border-color: var(--primary-hover) !important;
+    }
+
+    .btn-secondary {
+        background-color: var(--secondary) !important;
+        border-color: var(--secondary) !important;
+    }
+
+    .btn-secondary:hover {
+        background-color: var(--secondary-hover) !important;
+        border-color: var(--secondary-hover) !important;
+    }
+
+    .btn-custom-arrow {
+        position: relative;
+        transition: background-color 300ms ease-out;
+        background: linear-gradient(-45deg, var(--primary), var(--secondary), var(--primary));
+        background-size: 400%;
+        background-position: 90% 0;
+        color: #fff;
+        transition: background 0.8s;
+        border: none;
+    }
+
+    .btn-custom-arrow:hover {
+        background-position: 185% 50%;
+    }
+    .btn-custom-arrow span {
+        display: inline-block;
+        position: relative;
+        transition: all 300ms ease-out;
+        will-change: transform;
+    }
+    .btn-custom-arrow:hover span {
+        transform: translate3d(-1rem, 0, 0);
+    }
+    .btn-custom-arrow svg {
+        position: absolute;
+        width: 1.1em;
+        right: 0px;
+        right: 0rem;
+        opacity: 0;
+        top: 50%;
+        transform: translateY(-50%);
+        transition: all 300ms ease-out;
+        will-change: right, opacity;
+    }
+    .btn-custom-arrow svg * {
+        stroke-width: 5;
+        stroke-color: transparent;
+    }
+    .btn-custom-arrow:hover svg {
+        opacity: 1;
+        right: -2rem;
+    }
+
+    .button-charming {
+        height: 3rem;
+        /* border-radius: 0.rem; */
+        padding: 0 2rem;
+        background: linear-gradient(-45deg, var(--primary), var(--secondary), var(--primary));
+        background-size: 400%;
+        background-position: 90% 0;
+        color: #fff;
+        transition: background 0.8s;
+        border: none;
+    }
+
+    .button-charming:hover {
+        background-position: 185% 50%;
+    }
+
+    .btn {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 8px;
+        border: 1px solid transparent;
+        min-height: 3rem;
+    }
+
+    .btn-ghost {
+        background-color: var(--secondary-light);
+        border-color: var(--secondary);
+    }
+
+    .btn-ghost:hover {
+        background-color: var(--secondary);
+        border-color: var(--secondary);
+    }
+
+    .btn-outline {
+        background-color: transparent !important;
     }
 </style>
