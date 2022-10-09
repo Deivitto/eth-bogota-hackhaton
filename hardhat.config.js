c297e28fea2741eca263a76d60cc35ec57e5ef0a
Baraa42 <75384314+Baraa42@users.noreply.github.com>
2022-10-09 06:37:03 -0500

    config deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,33 +7,44 @@ require("solidity-coverage")
 require("dotenv").config()
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || ""
-const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY || ""
+const POLYGON_RPC_URL = process.env.POLYGON_RPC_URL || ""
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
 const GNOSIS_RPC_URL = process.env.GNOSIS_RPC_URL || ""
-const GNOSIS_PRIVATE_KEY = process.env.GNOSIS_PRIVATE_KEY || ""
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 
 module.exports = {
     defaultNetwork: "hardhat",
     networks: {
-        hardhat: {
-            chainId: 31337,
-            blockConfirmations: 1,
-        },
+        hardhat: {},
         goerli: {
             chainId: 5,
             blockConfirmations: 6,
             url: GOERLI_RPC_URL,
-            accounts: [GOERLI_PRIVATE_KEY],
+            accounts: [PRIVATE_KEY],
         },
-        gnosis : {
+        gnosis: {
             chainId: 100,
             blockConfirmations: 10,
             url: GNOSIS_RPC_URL,
-            accounts: [GNOSIS_PRIVATE_KEY],
+            accounts: [PRIVATE_KEY],
+        },
+        polygon: {
+            chainId: 137,
+            blockConfirmations: 3,
+            url: POLYGON_RPC_URL,
+            accounts: [PRIVATE_KEY],
         },
     },
-    solidity: "0.8.17",
+    solidity: {
+        version: "0.8.17",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            },
+        },
+    },
     namedAccounts: {
         deployer: {
             default: 0,
